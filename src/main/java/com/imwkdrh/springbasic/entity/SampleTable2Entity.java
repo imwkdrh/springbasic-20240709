2ce334dd0d115e86f67af151d84f59b94f2ec8fe
imwkdrh <imwkdrh7@naver.com>
2024-08-26 17:34:59 +0900

    2024-08-26 17:33

---
+++ src/main/java/com/imwkdrh/springbasic/entity/SampleTable2Entity.java
@@ -0,0 +1,25 @@
+package com.imwkdrh.springbasic.entity;
+
+import jakarta.annotation.Generated;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity(name = "st2")
+@Table(name = "sample_table_2")
+public class SampleTable2Entity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private Integer sampleAi;
+    private Boolean sampleColumn;
+}
