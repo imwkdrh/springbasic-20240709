14e9c0a1f18eabe2079b7c2916d0130a35089dc9
imwkdrh <imwkdrh7@naver.com>
2024-09-05 15:49:36 +0900

    2024-09-05 15:49

--- src/main/java/com/imwkdrh/springbasic/config/WebSecurityConfig.java
+++ src/main/java/com/imwkdrh/springbasic/config/WebSecurityConfig.java
@@ -80,12 +80,12 @@ public class WebSecurityConfig {
                 // - 모든 클라이언트가 접근할 수 있도록 허용
                 // - 인증된 모든 클라이언트가 접근할 수 있도록 허용
                 // - 인증된 클라이언트 중 특정 권한을 가진 클라이언트만 접근할 수 있도록 허용
-                .authorizeHttpRequests(ruquest -> ruquest
+                .authorizeHttpRequests(request -> request
                         // requestMatchers(): URL 패턴, HTTP 메서드 + URL 패턴, HTTP 메서드 마다 접근 허용 방식을 지정하는 메서드
                         // permitAll(): 모든 클라이언트가 접근할 수 있도록 지정
                         // hasRole(권한): 특정 권한을 가진 클라이언트만 접근할 수 있도록 지정
                         // authenticated(): 인증된 모든 클라이언트가 접근할 수 있도록 지정
-                        .requestMatchers("/anyone/**", "/auth/**").permitAll()
+                        .requestMatchers("/anyone/**", "/auth/**", "/oauth2/**" ).permitAll()
                         .requestMatchers(HttpMethod.GET, "/sample/jwt/*").permitAll()
                         .requestMatchers("/admin/**").hasRole("ADMIN")
                         .requestMatchers("/service").hasRole("ADMIN")
@@ -96,6 +96,12 @@ public class WebSecurityConfig {
                         .anyRequest().authenticated())
                 // JwtAuthenticationFilter를 UsernamePasswordAuthenticationFilter 이전에 등록
 
+                // OAuth2 인증 처리
+                .oauth2Login(
+                        oauth2 -> oauth2
+                                .redirectionEndpoint(endPoint -> endPoint
+                        .baseUri("/oauth2/callback/*"))
+                )
 
                 // 인증 및 인가 과정에서 발생한 예외를 직접 처리
                 .exceptionHandling(
